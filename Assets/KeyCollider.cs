fd55f9be5f4b3e6bf76e9535224a8090eb327c06
EmileTurbo <emilehoule222@gmail.com>
2023-11-01 13:52:04 -0400

    key update

---
+++ Assets/KeyCollider.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollider : MonoBehaviour
+{
+    public GameObject keyOnPlayer;
+    public GameObject porte;
+    bool isKey = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Key")
+        {
+            Destroy(other.gameObject);
+            keyOnPlayer.SetActive(true);
+            isKey = true;
+        }
+
+        if(other.gameObject.tag == "porte")
+        {
+            if(isKey == true)
+            {
+                porte.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
